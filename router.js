58dbc1bec25072d4e8707e0f3c78b68bf3fe21be
Mohammad Faisal Ali <codingfaisal@gmail.com>
2023-03-17 20:10:45 +0530

    Added routing

---
+++ router.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const { getTodos, createTodo } = require("./controllers/Todo");
+
+router.get("/", (req, res) => {
+  res.send("Let's build a CRUD API!");
+});
+
+router.get("/todos", getTodos);
+
+router.post("/todos", createTodo);
+
+
+module.exports = router;
